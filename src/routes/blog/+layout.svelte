bf849567b4cba7710ad25ac6c2ca84240e745542
Sunil Kumar <sunilpalarya@gmail.com>
2023-03-03 17:41:26 +0530

    removed unnecessary code

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -11,22 +11,17 @@
 
 	export let data = [];
 	var dups = [];
-	console.log("res from blog layout page: ", data.tags);
 	// Array to keep track of duplicates
 	filterArrayElements(data.tags);
-	console.log("dups after filterArrayElements: ", dups);
-
 	var arrStr = dups.join(",");
-	console.log("arrStr: ", arrStr);
-
 	var newArr = arrStr.split(",");
-	var { arr2 } = filterArrayElements2(newArr);
-	var filteredTags = arr2;
+	
+	var filteredTags = removeDuplicates(newArr);
+	function removeDuplicates(arr) {
+        return [...new Set(arr)];
+    }
 
-	console.log("filteredTags: ", filteredTags);
-	async function filterArrayElements(arrItems) {
-		console.log("tags inside filterArrayElements: ", arrItems);
-		
+	function filterArrayElements(arrItems) {
 		var arr = arrItems.filter(function (el) {
 			// If it is not a duplicate, return true
 			if (dups.indexOf(el.tag) == -1) {
@@ -36,22 +31,7 @@
 
 			return false;
 		});
-		console.log("dups inside filterArrayElements: ", dups);
 		return { dups, arr };
-	}
-	function filterArrayElements2(arrItems) {
-		var dups2 = [];
-		var arr2 = arrItems.filter(function (el) {
-			var item = el.toString().trim();
-			// If it is not a duplicate, return true
-			if (dups2.indexOf(item) == -1) {
-				dups2.push(item);
-				return true;
-			}
-
-			return false;
-		});
-		return { dups2, arr2 };
 	}
 </script>
 
@@ -65,7 +45,7 @@
 				{#if $LoggedInUser}
 					<a class="btn btn-primary mb-4" href="/blog/addblog">Add Blog</a>
 				{/if}
-				<h1>Blogs:</h1>
+				<h3>Blogs:</h3>
 				<BlogsList data={data.titles} />
 			</div>
 			<div class="col-12">
